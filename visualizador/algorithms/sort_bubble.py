a78f7e0e45ddfca3010ba7a3248bd7ff2ccbc4c8
Florencia Lazar <alazaflor@gmail.com>
2025-11-17 20:42:57 -0300

    Sort Bubble

--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -13,7 +13,29 @@ def init(vals):
     j = 0
 
 def step():
-    # TODO:
+    global items, n,i,j
+    if i >= n-1:
+        return {"done:True"}
+    a=j
+    b=j+1
+    swap=False
+
+    if items[a]>items[b]:
+        items[a],items[b]= items[b],items[a]
+        swap=True
+
+        j= j + 1
+
+        if j>=n-i-1:
+            i= i + 1
+            j=0
+            return{
+        "a":a,
+        "b":b,
+        "swap": swap,
+        "done": False,
+        "items": items,
+    }
     # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
     # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
     # 3) Avanzar punteros (preparar el próximo paso).
